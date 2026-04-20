fb9773b98f5f22bb4b0520626121d213693242b8
Andrii Dmtryk <admitruk237@gmail.com>
2026-04-20 14:47:23 +0200

    feat: create main bg

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,13 @@
+import headerSvg from '@/shared/assets/bg/header.svg';
+import citySvg from '@/shared/assets/bg/tokiocity.svg';
+import cloudSvg from '@/shared/assets/bg/cloud.svg';
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Tokio Slots</h1>
+    <div className="relative min-h-screen bg-linear-to-b from-bg-idle-from to-bg-idle-to overflow-hidden">
+      <img src={headerSvg} alt="" className="absolute top-0 left-0 w-full" />
+      <img src={citySvg} alt="" className="absolute left-0 w-full" style={{ bottom: '125.34px' }} />
+      <img src={cloudSvg} alt="" className="absolute bottom-0 left-0 w-full" />
     </div>
   );
 }
